03a6b9bbd4384ecfc37dc3f08e894400851c76d2
Josh Rose <1677846+JoshTheWanderer@users.noreply.github.com>
2025-11-10 23:18:34 +0100

    build: install husky conditionally

---
+++ .husky/install.mjs
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+if (process.env.NODE_ENV === 'production' || process.env.CI === 'true') {
+  process.exit(0)
+}
+
+const husky = (await import('husky')).default
+
+console.log(husky())
